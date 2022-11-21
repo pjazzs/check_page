b89288b35a9a1ac61b0616bb976aa99ff764b388
pjazzs <116002525+pjazzs@users.noreply.github.com>
2022-11-21 09:59:09 +0100

    Add files via upload

---
+++ peace/script.js
@@ -0,0 +1,31 @@
+console.log("Working")
+
+function add2Values(a, b){
+    return a + b;
+}
+
+console.log(add2Values(10, 5));
+
+
+// OR
+
+let Num1 = 10;
+let Num2 = 5;
+let sum  = Num1 + Num2;
+
+console.log(` The anwser is: ${sum}`);
+
+const itmes =[23, 90, 67, 89, 12]
+
+itmes.shift()
+
+console.log(itmes);
+
+itmes.pop()
+console.log(itmes)
+
+itmes.push(100)
+console.log(itmes)
+
+itmes.unshift()
+    console.log(itmes)
